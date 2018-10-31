a09f8f35e5c369dd83c716d317bc2e59cfd97f87
周诺华 <13605606825@163.com>
2018-10-31 18:00:31 +0800

    修改webpack配置

--- webpack.server.js
+++ webpack.server.js
@@ -2,26 +2,26 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    target:'node',
-    node:'development',
-    entry:'./src/index.js',
-    output: {
-        filename:'bundle.js',
-        path: path.resolve(__dirname, 'build')
-    },
-    externals: [nodeExternals()],
-    module: {
-        reles:[{
-            test: /\.js?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            options: {
-                presets: ['react', 'stage-0',['env', {
-                    targets: {
-                        browsers: ['last 2 versions']
-                    }
-                }]]
-            }
-        }]
-    }
+	target: 'node',
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build')
+	},
+	externals: [nodeExternals()],
+	module: {
+		rules: [{
+			test: /\.js?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			options: {
+				presets: ['react', 'stage-0', ['env', {
+					targets: {
+						browsers: ['last 2 versions']
+					}
+				}]]
+			}
+		}]
+	}
 }
