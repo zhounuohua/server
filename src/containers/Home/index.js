142edc4237e0f92869fc33771ffdaebd5fcd94c6
周诺华 <13605606825@163.com>
2018-10-31 16:36:15 +0800

    安装React

---
+++ src/containers/Home/index.js
@@ -0,0 +1,20 @@
+const React =  require('react');
+
+const Hemo = () => {
+    return <div>hemo</div>
+}
+
+module.exports = {
+    default : Home
+}
+
+
+
+
+// import React from 'react';
+
+// const Hemo = () => {
+//     return <div>hemo</div>
+// }
+
+// export default Hemo;
