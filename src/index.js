06b321276781f2d972dde07d68dcd758c69280ca
周诺华 <13605606825@163.com>
2018-10-31 17:49:35 +0800

    安装 webpack。  npm install webpack webpack-cli --save
    npm install babel-loader babel-core --save
    
    npm install babel-preset-react --save
    
    npm install babel-preset-stage-0
    
    npm install babel-preset-env --save
    
    npm install webpack-node-externals --save

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const Home = require('./containers/Home');
 
 app.get('/', (req, res) => res.send(`
     <html>
---
+++ webpack.server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    target:'node',
+    node:'development',
+    entry:'./src/index.js',
+    output: {
+        filename:'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        reles:[{
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+                presets: ['react', 'stage-0',['env', {
+                    targets: {
+                        browsers: ['last 2 versions']
+                    }
+                }]]
+            }
+        }]
+    }
+
+}
