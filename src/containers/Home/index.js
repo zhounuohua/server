7ddd40022835ff313a4be0586ad22439f2398503
周诺华 <13605606825@163.com>
2018-11-06 12:02:51 +0800

    自动运行打包服务器重启

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-	return <div>home</div>
-}
+	return (
+	<div>
+		<div>hello dell lee!</div>
+		<button onClick={()=>{alert("click")}}>click</button>
+	</div>
+	)}
 
 export default Home;
